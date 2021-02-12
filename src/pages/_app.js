bec1a9f85de6a3a60beee8dd2738b4806172a4b7
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-02-12 12:09:44 +0100

    feat: added matomo analytics #64

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,21 @@
 import '@/styles/index.scss'
 import Layout from '@/components/layout'
+import { useEffect } from 'react'
+import { init } from '../lib/matomo'
+
+const matomoUrl = 'https://matomo.emkanrecords.com/'
+const matomoId = '6'
 
 export default function MyApp ({ Component, pageProps }) {
+
+    useEffect (() => {
+
+        init ({
+            'url': matomoUrl,
+            'siteId': matomoId,
+        })
+
+    }, [])
 
     return (
         <Layout>
