99a7436150d928e3913c3d815b5c990428cb49ec
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-02-12 12:15:32 +0100

    fix: better matomo implementation #64

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,15 @@
 import '@/styles/index.scss'
 import Layout from '@/components/layout'
 import { useEffect } from 'react'
-import { init } from '../lib/matomo'
-
-const matomoUrl = 'https://matomo.emkanrecords.com/'
-const matomoId = '6'
+import { init } from '@/lib/matomo'
 
 export default function MyApp ({ Component, pageProps }) {
 
     useEffect (() => {
 
         init ({
-            'url': matomoUrl,
-            'siteId': matomoId,
+            'url': 'https://matomo.emkanrecords.com/',
+            'siteId': '6',
         })
 
     }, [])
