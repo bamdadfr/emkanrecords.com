e2076c229bf57c4da3a41c9e8f3fab073fdb881f
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-19 23:20:06 +0200

    fix(layout-meta): missing space
    [force-release]

--- src/components/layout-meta/layout-meta.component.js
+++ src/components/layout-meta/layout-meta.component.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
-function LayoutMetaComponent() {
-
+function LayoutMetaComponent () {
+        
     return (
         <Head>
             <link
