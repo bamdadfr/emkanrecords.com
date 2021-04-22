f275ee0e8c64b19af00a56e4e32c068f3a25bd17
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-22 13:29:26 +0200

    fix(_document): disabling eslint

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { GA_TRACKING_ID } from '@/lib/gtag'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+import {GA_TRACKING_ID} from '@/lib/gtag'
 
 export default class MyDocument extends Document {
 
-    render () {
+    render() {
 
         return (
             <Html lang="en">
@@ -15,12 +16,12 @@ export default class MyDocument extends Document {
                         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
                     />
                     <script
-                        dangerouslySetInnerHTML={{ '__html': `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${GA_TRACKING_ID}",{page_path:window.location.pathname});` }}
+                        dangerouslySetInnerHTML={{'__html': `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${GA_TRACKING_ID}",{page_path:window.location.pathname});`}}
                     />
                 </Head>
                 <body>
-                    <Main/>
-                    <NextScript/>
+                <Main/>
+                <NextScript/>
                 </body>
             </Html>
         )
