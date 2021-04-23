2ad355e34bf87b5a1ddf8fc6e2644d1459612618
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-23 15:42:28 +0200

    feat(sentry): added sentry for next.js [force-release]

---
+++ sentry.client.config.js
@@ -0,0 +1,14 @@
+// This file configures the intialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+
+Sentry.init ({
+    'dsn': SENTRY_DSN || 'https://8ea9d4e4198b43a8b0cf39c138683faf@o574972.ingest.sentry.io/5733370',
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+})
