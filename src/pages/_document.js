1442121616531e2ce2b21cdea5fa6f2f327b6442
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-04-26 16:53:52 +0200

    fix(_document): enabled eslint

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,59 +1,59 @@
-/* eslint-disable */
 import React from 'react'
-import Document, {Html, Head, Main, NextScript} from 'next/document'
-import {GA_TRACKING_ID} from '@/lib/gtag'
-import {Helmet} from "react-helmet";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '@/lib/gtag'
+import { Helmet } from 'react-helmet'
 
 export default class MyDocument extends Document {
 
-    static async getInitialProps(...args) {
-        const documentProps = await super.getInitialProps(...args)
+    static async getInitialProps (...args) {
+
+        const documentProps = await super.getInitialProps (...args)
+
         return {
             ...documentProps,
-            helmet: Helmet.renderStatic()
+            'helmet': Helmet.renderStatic (),
         }
+
     }
 
     // should render on <html>
-    get helmetHtmlAttrComponents() {
-        return this.props.helmet.htmlAttributes.toComponent()
+    get helmetHtmlAttrComponents () {
+
+        return this.props.helmet.htmlAttributes.toComponent ()
+
     }
 
     // should render on <body>
-    get helmetBodyAttrComponents() {
-        return this.props.helmet.bodyAttributes.toComponent()
+    get helmetBodyAttrComponents () {
+
+        return this.props.helmet.bodyAttributes.toComponent ()
+
     }
 
     // should render on <head>
-    get helmetHeadComponents() {
-        return Object.keys(this.props.helmet)
-            .filter((el) => el !== 'htmlAttributes' && el !== 'bodyAttributes')
-            .map((el) => this.props.helmet[el].toComponent())
+    get helmetHeadComponents () {
+
+        return Object.keys (this.props.helmet)
+            .filter ((el) => el !== 'htmlAttributes' && el !== 'bodyAttributes')
+            .map ((el) => this.props.helmet[el].toComponent ())
+
     }
 
-
-    render() {
+    render () {
 
         return (
-            <Html
-                {...this.helmetHtmlAttrComponents}
-            >
+            <Html {...this.helmetHtmlAttrComponents}>
                 <Head>
                     {this.helmetHeadComponents}
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
                     <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{'__html': `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${GA_TRACKING_ID}",{page_path:window.location.pathname});`}}
+                        dangerouslySetInnerHTML={{ '__html': `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${GA_TRACKING_ID}",{page_path:window.location.pathname});` }}
                     />
                 </Head>
-                <body
-                    {...this.helmetBodyAttrComponents}
-                >
-                <Main/>
-                <NextScript/>
+                <body {...this.helmetBodyAttrComponents}>
+                    <Main/>
+                    <NextScript/>
                 </body>
             </Html>
         )
