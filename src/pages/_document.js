2c49a484fbd559211f85c27bb40e24bab83b13df
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-05 13:29:30 +0200

    fix(_document): disabling eslint
    [force-release]

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { GA_TRACKING_ID } from '@/lib/gtag'
