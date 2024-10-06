b5850efde8a503ba9ffd7d3cb793e0e4c2984dc7
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-06 19:52:21 +0200

    fix: remove sentry

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-const {withSentryConfig} = require('@sentry/nextjs');
-
-const moduleExports = {
+/** @type {import('next').NextConfig} */
+const config = {
   poweredByHeader: false,
   outputFileTracing: false,
 };
 
-const SentryWebpackPluginOptions = {
-  hideSourceMaps: true,
-};
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+export default config;
